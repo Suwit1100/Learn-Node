bbebae6fd7762a3af29b124be201bf626b2d1523
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-25 14:30:35 +0700

    module

---
+++ module.js
@@ -0,0 +1,8 @@
+const mymodule = require('./module/mymodule')
+
+const sumnumber = mymodule.sumNumber(10, 50)
+console.log(sumnumber);
+
+
+const fullname = mymodule.sumText("สุวิทย์", "ลีเจ้ย")
+console.log(fullname);
---
+++ module/mymodule.js
@@ -0,0 +1,9 @@
+function sumNumber(x, y) {
+    return x + y
+}
+
+function sumText(fname, lname) {
+    return `${fname} ${lname}`
+}
+
+module.exports = { sumNumber, sumText };
